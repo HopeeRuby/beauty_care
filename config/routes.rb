bf8181f31d0c6284fcc3805627cf3fc7f72e1ec1
Pham Ngoc Anh <anhpn@devcamp.edu.vn>
2023-04-13 08:52:29 +0700

    split layout into admin and frontend

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@
 
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :admin do
+    # các route cho trang admin
+    resources :admins
+  end
+  namespace :frontend do
+    # các route cho phía frontend
+    resources :users
+  end
 end
