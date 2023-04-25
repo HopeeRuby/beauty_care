1cc1345c703a32612f34def0e3b613c51d57b2d0
nththam <hongthamnguyem2k@gmail.com>
2023-04-25 12:41:40 +0700

    customer_layout_devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,21 @@
 class ApplicationController < ActionController::Base
   protected
 
+  layout :layout_by_resource
+
   def after_sign_in_path_for(resource)
     resource.instance_of?(Admin) ? administrator_root_path : '/'
   end
 
   def after_sign_out_path_for(scope)
     scope == :admin ? new_admin_session_path : '/'
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
   end
 end
