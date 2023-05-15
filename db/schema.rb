af70f67f72b8c4d26e99706124125069d9d0e6bb
anhpn0712 <ngocanh19051993@gmail.com>
2023-05-15 15:33:16 +0700

    Feature - Install gem paranoia and set make sure only admin without soft-delete can login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_035324) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_074714) do
   create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name", default: ""
     t.string "last_name", default: ""
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_035324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_admins_on_deleted_at"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
